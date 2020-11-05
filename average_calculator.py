531e6eefd66e4dc3b083fa13a95765c629ee5335
Kyle <68929012+khalmklowd@users.noreply.github.com>
2020-11-05 18:44:25 -0500

    Add files via upload
    updated version of the average calculator.

--- average_calculator.py
+++ average_calculator.py
@@ -1,16 +1,20 @@
-'''Ask for the numbers to find the average of'''
-numbers = input("What are the numbers to calculate?")
+while True:
+	'''Ask for the numbers to find the average of'''
+	numbers = input("Enter the numbers to average.\nEnter 'q' to quit. ")
+	if numbers.lower() == 'q':
+		break
+	else:	
+		'''convert input string to list of strings.'''
+		numbers = numbers.split()
 
-'''convert input string to list of strings.'''
-numbers = numbers.split()
+		'''convert list of strings to list of floats.'''
+		lst = [float(x) for x in numbers]
 
-'''convert list of strings to list of floats.'''
-lst = [float(x) for x in numbers]
+		'''use input to formulate the average of the given numbers.'''
 
-'''use input to formulate the average of the given numbers.'''
+		total = sum(lst)
+		div = len(lst)
+		ave = total/div
 
-total = sum(lst)
-div = len(lst)
-ave = total/div
-
-print(f"The average of your numbers is: {ave}")
+		'''Print the result.'''
+		print(f"The average of your numbers is: {ave}")
